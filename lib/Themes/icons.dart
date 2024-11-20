8166173ca4315f26c13e3347ec423ed3af23f081
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-20 16:44:06 +0000

    add icon

---
+++ lib/Themes/icons.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+class myIcons {
+  static Icon addIcon = Icon(Icons.add, size: 32,);
+}
--- lib/features/home/widgets/addButton.dart
+++ lib/features/home/widgets/addButton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tasks/Themes/icons.dart';
 
 class addButton extends StatefulWidget {
   const addButton({
@@ -14,7 +15,7 @@ class _addButtonState extends State<addButton> {
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: () { },
-      icon: Icon(Icons.add, size: 32,),
+      icon: myIcons.addIcon,
     );
   }
 }
