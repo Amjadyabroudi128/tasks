74673f0badb30cea107d6b7d7c1e7e3ce968c021
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-20 16:11:27 +0000

    removed comment

--- lib/features/home/homeScreen.dart
+++ lib/features/home/homeScreen.dart
@@ -34,16 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // body: Center(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       const Text(
-      //         'You have pushed the button this many times:',
-      //       ),
-      //     ],
-      //   ),
-      // ),
     );
   }
 }
