8f878c505c63dcf6d1a08a2f6e3948cfe9c87a3b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-21 16:34:43 +0000

    color helper

--- lib/Themes/appTheme.dart
+++ lib/Themes/appTheme.dart
@@ -1,11 +1,12 @@
 
  import 'package:flutter/material.dart';
+import 'package:tasks/Themes/colors.dart';
 
 class AppTheme {
  static final myTheme = ThemeData().copyWith(
    iconTheme: IconThemeData(
      size: 32,
-     color: Colors.black
+     color: ColorHelper.addIcon
    )
  );
  }
