795b982f4fd9b31594aaeacea518f02121f1b5d5
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-21 16:54:06 +0000

    functions

--- lib/features/home/homeScreen.dart
+++ lib/features/home/homeScreen.dart
@@ -15,6 +15,32 @@ class _MyHomePageState extends State<MyHomePage> {
   final mySupabase DB = mySupabase();
   List<dynamic> myTasks = [];
 
+  Future<void> fetch() async {
+    final tasks = await DB.getTasks();
+    setState(() {
+      myTasks = tasks;
+    });
+  }
+  Future<void> addTask(String taskName)async {
+    await DB.addTask(taskName);
+    myController.clear();
+    fetch();
+  }
+  Future<void> _toggleTask(int id, bool isCompleted) async {
+    await DB.toggleTask(id, isCompleted);
+    fetch(); // Refresh the task list
+  }
+
+  Future<void> _deleteTask(int id) async {
+    await DB.deleteTask(id);
+    fetch(); // Refresh the task list
+  }
+
+  Future<void> _editTask(int id, String updatedTask) async {
+    await DB.editTask(id, updatedTask);
+    fetch(); // Refresh the task list
+  }
+
   @override
   Widget build(BuildContext context) {
     return  const Scaffold(
