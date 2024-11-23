86e7a006759884db902e59e4c408413bb1d944a0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-23 19:51:21 +0000

    input decoration theme

--- lib/Themes/appTheme.dart
+++ lib/Themes/appTheme.dart
@@ -3,17 +3,19 @@
 import 'package:tasks/Themes/colors.dart';
 
 class AppTheme {
+  static final border = OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+          Radius.circular(13)
+      )
+  );
  static final myTheme = ThemeData().copyWith(
    iconTheme: IconThemeData(
      size: 32,
      color: ColorHelper.addIcon
    ),
    inputDecorationTheme: InputDecorationTheme(
-     border: OutlineInputBorder(
-         borderRadius: BorderRadius.all(
-             Radius.circular(13)
-         )
-     ),
+     border: border,
+     focusedBorder: border
    )
  );
  }
