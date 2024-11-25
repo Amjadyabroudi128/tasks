e7fdee9e455037788146ad6ed42d26a70f1b48d1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-25 14:30:26 +0000

    filled Color

--- lib/Themes/colors.dart
+++ lib/Themes/colors.dart
@@ -5,4 +5,5 @@ class ColorHelper {
   static Color addIcon = Colors.black;
   static Color focusClr = Colors.grey;
   static Color selectedClr = Colors.green;
+  static Color filledClr = Colors.white;
  }
