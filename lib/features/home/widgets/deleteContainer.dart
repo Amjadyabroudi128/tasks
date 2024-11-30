58c927ba85e68bc9bd7579078c111f0545a2232d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-30 13:59:57 +0000

    delete icon

--- lib/features/home/widgets/deleteContainer.dart
+++ lib/features/home/widgets/deleteContainer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tasks/Themes/colors.dart';
+import 'package:tasks/Themes/icons.dart';
 
 class deleteContainer extends StatelessWidget {
   const deleteContainer({
@@ -10,7 +11,7 @@ class deleteContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: ColorHelper.delete,
-      child: Icon(Icons.delete, color: Colors.white,),
+      child: myIcons.deleteIcon,
     );
   }
 }
