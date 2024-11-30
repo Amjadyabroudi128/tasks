d5adea640bc5fe79ad0378438d36ada27a2cd5e7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-30 14:00:30 +0000

    delete icon color

--- lib/Themes/icons.dart
+++ lib/Themes/icons.dart
@@ -1,7 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:tasks/Themes/colors.dart';
 
 class myIcons {
   static Icon addIcon = Icon(Icons.add,);
-  static Icon deleteIcon = Icon(Icons.delete, color: Colors.white,);
+  static Icon deleteIcon = Icon(Icons.delete, color: ColorHelper.filledClr);
 }
