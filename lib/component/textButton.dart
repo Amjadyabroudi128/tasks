90ab6e07a66519c7bdf776be33c72ace9b4d1fa7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-01 13:51:22 +0000

    textBtn component

---
+++ lib/component/textButton.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class kTextBtn extends StatelessWidget {
+  const kTextBtn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
