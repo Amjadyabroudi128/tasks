cdd8065c6f044b1ac1f9f1285493106ab481b125
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-04 15:26:37 +0000

    slide icon

--- lib/Themes/icons.dart
+++ lib/Themes/icons.dart
@@ -5,4 +5,5 @@ import 'package:tasks/Themes/colors.dart';
 class myIcons {
   static Icon addIcon = Icon(Icons.add,);
   static Icon deleteIcon = Icon(Icons.delete, color: ColorHelper.filledClr);
+  static Icon slide = Icon(Icons.arrow_back, color: Colors.grey, size: 29);
 }
