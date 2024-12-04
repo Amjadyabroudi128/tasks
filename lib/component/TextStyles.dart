636686b675a8a7af817f0cc731df9e80b42c7ad3
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-04 15:52:24 +0000

    slide Text color

--- lib/component/TextStyles.dart
+++ lib/component/TextStyles.dart
@@ -1,11 +1,12 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:tasks/Themes/colors.dart';
 import 'package:tasks/Themes/fontWeight.dart';
 
 class myTextStyle {
   static TextStyle taskStyle = TextStyle(fontSize: 25, fontWeight: fontWeightHelper.bold,
   );
   static TextStyle empty = TextStyle(fontSize: 24);
-  static TextStyle slide = TextStyle(fontSize: 12, color: Colors.grey);
+  static TextStyle slide = TextStyle(fontSize: 12, color: ColorHelper.slideT);
  }
