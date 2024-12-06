afd4408ca2f07c888bdd80b56b3c03d7d8d05813
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-06 17:26:15 +0000

    font size

--- lib/component/TextStyles.dart
+++ lib/component/TextStyles.dart
@@ -3,11 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:tasks/Themes/colors.dart';
 import 'package:tasks/Themes/fontWeight.dart';
+import 'package:tasks/fontSizes.dart';
 
 class myTextStyle {
   static TextStyle taskStyle = TextStyle(fontSize: 25, fontWeight: fontWeightHelper.bold,
   );
-  static TextStyle empty = TextStyle(fontSize: 24);
+  static TextStyle empty = TextStyle(fontSize: myFonts.empty24);
   static TextStyle slide = TextStyle(fontSize: 12, color: ColorHelper.slideT);
   static TextStyle dismiss = TextStyle(fontSize: 20, color: ColorHelper.addIcon);
   static TextStyle confirm = TextStyle(fontSize: 20, color: ColorHelper.confirm);
