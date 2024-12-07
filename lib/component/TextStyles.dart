158b44fdf6f0fa1c1ad36bac0cdaeed752774635
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-07 18:35:46 +0000

    S12 font

--- lib/component/TextStyles.dart
+++ lib/component/TextStyles.dart
@@ -9,7 +9,7 @@ class myTextStyle {
   static TextStyle taskStyle = TextStyle(fontSize: 25, fontWeight: fontWeightHelper.bold,
   );
   static TextStyle empty = TextStyle(fontSize: myFonts.empty24);
-  static TextStyle slide = TextStyle(fontSize: 12, color: ColorHelper.slideT);
+  static TextStyle slide = TextStyle(fontSize: myFonts.S12, color: ColorHelper.slideT);
   static TextStyle dismiss = TextStyle(fontSize: myFonts.CD20, color: ColorHelper.addIcon);
   static TextStyle confirm = TextStyle(fontSize: myFonts.CD20, color: ColorHelper.confirm);
  }
