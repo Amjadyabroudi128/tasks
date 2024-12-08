b51c277068275f4cb139d6832602ed013bc6b30e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-08 17:41:06 +0000

    slide icon size

--- lib/Themes/icons.dart
+++ lib/Themes/icons.dart
@@ -1,9 +1,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:tasks/Themes/colors.dart';
+import 'package:tasks/fontSizes.dart';
 
 class myIcons {
   static Icon addIcon = Icon(Icons.add,);
   static Icon deleteIcon = Icon(Icons.delete, color: ColorHelper.filledClr);
-  static Icon slide = Icon(Icons.arrow_back, color: ColorHelper.slideT, size: 29);
+  static Icon slide = Icon(Icons.arrow_back, color: ColorHelper.slideT, size: myFonts.sIcon);
 }
