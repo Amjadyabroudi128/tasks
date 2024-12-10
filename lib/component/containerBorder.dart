0d3450ec264397fe5d6797a7399afc401acdb307
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-10 14:47:34 +0000

    container bottom

---
+++ lib/component/containerBorder.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class containerBorder {
+ static Border btm = Border(
+     bottom: BorderSide(
+         color: Colors.black,
+         width: 2,
+         style: BorderStyle.solid
+     ),
+     right: BorderSide(
+         color: Colors.black,
+         width: 2
+     )
+ );
+ }
