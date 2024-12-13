d3c84ba83da8fb84f47aab7f1671bbce124b744c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-13 16:08:43 +0000

    padding card margin

--- lib/component/paddings.dart
+++ lib/component/paddings.dart
@@ -3,4 +3,5 @@
 class myPadding {
   static EdgeInsetsGeometry home = EdgeInsets.only(top: 39, left: 15, right: 15);
   static EdgeInsetsGeometry btm = EdgeInsets.only(bottom: 10);
+  static EdgeInsetsGeometry card = EdgeInsets.symmetric(vertical: 4);
  }
