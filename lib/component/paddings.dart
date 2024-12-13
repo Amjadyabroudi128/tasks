1273f9e0f3627d3074ea369255f1aeb876b92ee7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-13 16:09:52 +0000

    min axix size

--- lib/component/paddings.dart
+++ lib/component/paddings.dart
@@ -4,4 +4,5 @@ class myPadding {
   static EdgeInsetsGeometry home = EdgeInsets.only(top: 39, left: 15, right: 15);
   static EdgeInsetsGeometry btm = EdgeInsets.only(bottom: 10);
   static EdgeInsetsGeometry card = EdgeInsets.symmetric(vertical: 4);
+  static MainAxisSize min = MainAxisSize.min;
  }
