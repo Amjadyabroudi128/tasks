fe2eb18a501ca238c76d881f80edd049f533ba0b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-17 16:54:57 +0000

    if task is empty or hasn't changed

--- lib/features/home/widgets/saveDialog.dart
+++ lib/features/home/widgets/saveDialog.dart
@@ -3,12 +3,15 @@ import 'package:tasks/component/Strings.dart';
 import 'package:tasks/component/TextStyles.dart';
 import 'package:tasks/component/snackBar.dart';
 import 'package:tasks/component/textButton.dart';
-
 import '../../../component/TextFiled.dart';
 import '../../../supabase/CRUD.dart';
 
 Future<dynamic> myDialog(
-    BuildContext context, TextEditingController _editController, task, Function fetchTasks,) {
+    BuildContext context,
+    TextEditingController _editController,
+    Map<String, dynamic> task,
+    Function fetchTasks,
+    ) {
   final mySupabase _mySupabase = mySupabase();
 
   Future<void> _editTask(int id, String updatedTask) async {
@@ -29,18 +32,26 @@ Future<dynamic> myDialog(
           onPressed: () {
             Navigator.pop(context);
           },
-          child: Text(Strings.cancel, style: myTextStyle.cancel,),
+          child: Text(
+            Strings.cancel,
+            style: myTextStyle.cancel,
+          ),
         ),
         kTextBtn(
           onPressed: () async {
-            if (_editController.text.isNotEmpty) {
-              await _editTask(task['id'], _editController.text);
-              Navigator.pop(context);
-            } else {
+            if (_editController.text.trim().isEmpty) {
               showSnackBar(context, Strings.noEmpty);
+            } else if (_editController.text.trim() == task['task'].toString().trim()) {
+              showSnackBar(context, "Change the task");
+            } else {
+              await _editTask(task['id'], _editController.text.trim());
+              Navigator.pop(context);
             }
           },
-          child: Text(Strings.save, style: myTextStyle.save,),
+          child: Text(
+            Strings.save,
+            style: myTextStyle.save,
+          ),
         ),
       ],
     ),
