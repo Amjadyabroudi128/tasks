138f6883f09c4aff2b0e3f4a7348edaab79a18a2
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-21 14:49:23 +0000

    cancel

--- lib/fontSizes.dart
+++ lib/fontSizes.dart
@@ -6,7 +6,7 @@
   static double sIcon = 29;
   static double defaultIcon = 32;
   static double save = 19;
-  static double cancel = 10;
+  static double cancel = 15;
   static double thickness = 2.8;
   static double btmBorder = 3.2;
   static double mWidth = 2;
