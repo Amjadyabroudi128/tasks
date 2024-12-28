7a9a063a85c95dff4985a216eace0328166fb7ec
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-12-28 14:33:20 +0000

    floating

--- lib/component/TextStyles.dart
+++ lib/component/TextStyles.dart
@@ -17,4 +17,5 @@ class myTextStyle {
   static TextStyle cancel = TextStyle(fontSize: myFonts.cancel, color: ColorHelper.addIcon);
   static TextStyle sure = TextStyle(fontSize: myFonts.font18, fontWeight: fontWeightHelper.bold, color: ColorHelper.unSelected);
   static TextStyle label = TextStyle(fontSize: 16, color: Colors.grey[800]);
+  static TextStyle floating = TextStyle(fontSize: 19, color: Colors.grey[700]);
  }
